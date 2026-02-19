19274378c47d6fac02074e5597d1448c01d66d6d
hoerul.dev123 <hoerul.dev123@gmail.com>
2026-02-19 12:35:57 +0700

    add googel analytics

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Google Analytics opt-out snippet added by Site Kit -->
 <script>
-    window["ga-disable-G-KXMWEJ37TX"] = true;
+    window["G-KXMWEJ37TX"] = true;
 </script>
 <!-- End Google Analytics opt-out snippet added by Site Kit -->
 <title><?= esc($title ?? 'All Data International') ?></title>
